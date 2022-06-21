65bc0528fb8312392efc84de712e57fe19b1f1da
Tiago Angelo <kurtis.angelo@gmail.com>
2022-06-21 13:50:05 -0300

    Environment test

---
+++ env/.terraform.lock.hcl
@@ -0,0 +1,50 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/fluxcd/flux" {
+  version     = "0.15.1"
+  constraints = ">= 0.15.1, < 0.16.0"
+  hashes = [
+    "h1:VD67NqStsGq/DuESBVSE5xZC2LofqvuWhMG4WrYSzmo=",
+  ]
+}
+
+provider "registry.terraform.io/gavinbunney/kubectl" {
+  version     = "1.14.0"
+  constraints = ">= 1.14.0, < 1.15.0"
+  hashes = [
+    "h1:gLFn+RvP37sVzp9qnFCwngRjjFV649r6apjxvJ1E/SE=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/aws" {
+  version     = "4.18.0"
+  constraints = ">= 4.18.0, < 4.19.0"
+  hashes = [
+    "h1:6gkWNVTtIlpniC188RP43f9PrcXG9v5CqQS2xw1dVgM=",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/kubernetes" {
+  version     = "2.11.0"
+  constraints = ">= 2.11.0, < 2.12.0"
+  hashes = [
+    "h1:pJiAJwZKUaoAJ4x+3ONJkwEVkjrwGROCGFgj7noPO58=",
+  ]
+}
+
+provider "registry.terraform.io/kreuzwerker/docker" {
+  version     = "2.16.0"
+  constraints = ">= 2.16.0, < 2.17.0"
+  hashes = [
+    "h1:OcTn2QyCQNjDiJYy1vqQFmz2dxJdOF/2/HBXBvGxU2E=",
+  ]
+}
+
+provider "registry.terraform.io/tehcyx/kind" {
+  version     = "0.0.12"
+  constraints = ">= 0.0.12, < 0.1.0"
+  hashes = [
+    "h1:rmC++QY2+7N4DXL8CPYz8RLzTUzpZFMZjQviApOjPSE=",
+  ]
+}
---
+++ env/jaeger.tf
@@ -0,0 +1,170 @@
+resource "kubectl_manifest" "jaeger_helm_repository" {
+  yaml_body = <<YAML
+apiVersion: source.toolkit.fluxcd.io/v1beta1
+kind: HelmRepository
+metadata:
+  name: jaeger
+  namespace: ${kubernetes_namespace.jaeger.metadata[0].name}
+spec:
+  interval: ${local.flux.default_interval}
+  url: https://jaegertracing.github.io/helm-charts
+YAML
+
+  depends_on = [kubectl_manifest.fluxcd]
+}
+
+resource "kubectl_manifest" "jaeger_operator_helm_release" {
+  yaml_body = <<YAML
+apiVersion: helm.toolkit.fluxcd.io/v2beta1
+kind: HelmRelease
+metadata:
+  name: jaeger-operator
+  namespace: ${kubernetes_namespace.jaeger.metadata[0].name}
+spec:
+  interval: ${local.flux.default_interval}
+  chart:
+    spec:
+      chart: jaeger-operator
+      sourceRef:
+        kind: HelmRepository
+        name: jaeger
+  values:
+    rbac:
+      clusterRole: true
+  dependsOn:
+    - name: cert-manager
+      namespace: ${kubernetes_namespace.cert_manager.metadata[0].name}
+YAML
+
+  depends_on = [
+    kubectl_manifest.fluxcd,
+    kubectl_manifest.jaeger_helm_repository
+  ]
+}
+
+resource "kubectl_manifest" "jaeger" {
+  yaml_body = <<YAML
+apiVersion: jaegertracing.io/v1
+kind: Jaeger
+metadata:
+  name: jaeger
+  namespace: ${kubernetes_namespace.jaeger.metadata[0].name}
+spec:
+  ingress:
+    enabled: true
+    hosts:
+      - ${local.jaeger.query.host}
+    ingressClassName: traefik
+  storage:
+    type: memory
+  strategy: allinone
+  allInOne:
+    image: jaegertracing/all-in-one:1.35.2
+YAML
+
+  depends_on = [
+    kubectl_manifest.fluxcd,
+    kubectl_manifest.jaeger_operator_helm_release,
+    kubernetes_job_v1.wait_jaeger_crds
+  ]
+}
+
+resource "kubectl_manifest" "jaeger_collector_ingress" {
+  yaml_body = <<YAML
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: jaeger-collector
+  namespace: ${kubernetes_namespace.jaeger.metadata[0].name}
+spec:
+  ingressClassName: traefik
+  rules:
+    - host: ${local.jaeger.collector.host}
+      http:
+        paths:
+          - backend:
+              service:
+                name: jaeger-collector
+                port:
+                  number: 14268
+            pathType: ImplementationSpecific
+YAML
+
+  depends_on = [kubectl_manifest.jaeger]
+}
+
+resource "kubernetes_job_v1" "wait_jaeger_crds" {
+  metadata {
+    name      = "wait-jaeger-crds"
+    namespace = kubernetes_namespace.jaeger.metadata[0].name
+  }
+  spec {
+    template {
+      metadata {}
+      spec {
+        service_account_name = kubernetes_service_account_v1.jaeger_kubectl.metadata[0].name
+        container {
+          name  = "kubectl"
+          image = "docker.io/bitnami/kubectl:${data.kubectl_server_version.current.major}.${data.kubectl_server_version.current.minor}"
+          args  = ["wait", "--for=condition=Ready", "helmrelease/jaeger-operator", "--timeout", local.default_timeouts]
+        }
+        restart_policy = "Never"
+      }
+    }
+  }
+  wait_for_completion = true
+
+  timeouts {
+    create = local.default_timeouts
+    update = local.default_timeouts
+  }
+
+  depends_on = [
+    kubernetes_role_binding_v1.kubectl_jaeger_helmreleases_reader,
+    kubectl_manifest.jaeger_operator_helm_release,
+  ]
+}
+
+data "kubectl_server_version" "current" {
+  depends_on = [kind_cluster.otel]
+}
+
+resource "kubernetes_service_account_v1" "jaeger_kubectl" {
+  metadata {
+    name      = "kubectl"
+    namespace = kubernetes_namespace.jaeger.metadata[0].name
+  }
+}
+
+resource "kubernetes_role_v1" "jaeger_helmreleases_reader" {
+  metadata {
+    name      = "jaeger-helmreleases-reader"
+    namespace = kubernetes_namespace.jaeger.metadata[0].name
+  }
+  rule {
+    api_groups = ["helm.toolkit.fluxcd.io"]
+    resources  = ["helmreleases"]
+    verbs      = ["get", "list", "watch"]
+  }
+}
+
+resource "kubernetes_role_binding_v1" "kubectl_jaeger_helmreleases_reader" {
+  metadata {
+    name      = "kubectl-jaeger-helmreleases-reader"
+    namespace = kubernetes_namespace.jaeger.metadata[0].name
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "Role"
+    name      = kubernetes_role_v1.jaeger_helmreleases_reader.metadata[0].name
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = kubernetes_service_account_v1.jaeger_kubectl.metadata[0].name
+    namespace = kubernetes_namespace.jaeger.metadata[0].name
+  }
+}
+
+resource "kubernetes_namespace" "jaeger" {
+  metadata { name = var.jaeger_namespace }
+}
