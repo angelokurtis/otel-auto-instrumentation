d145a4281623eb6c5d9060695b4272046af70fc2
Tiago Angelo <kurtis.angelo@gmail.com>
2022-06-21 14:16:52 -0300

    Generate an aleatory book response

--- apps/library/src/main/java/com/kurtis/library/Book.java
+++ apps/library/src/main/java/com/kurtis/library/Book.java
@@ -1,8 +1,13 @@
 package com.kurtis.library;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class Book {
-
+    private String author;
+    private String title;
+    private String publisher;
+    private String genre;
 }
--- apps/library/src/main/java/com/kurtis/library/LibraryController.java
+++ apps/library/src/main/java/com/kurtis/library/LibraryController.java
@@ -1,17 +1,28 @@
 package com.kurtis.library;
 
+import com.github.javafaker.Faker;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class LibraryController {
+
+    private final Faker faker;
+
     @RequestMapping(value = "/books", method = RequestMethod.GET)
     public List<Book> readBooks() {
-
-        return List.of(new Book());
+        com.github.javafaker.Book fakeBook = this.faker.book();
+        return List.of(Book.builder()
+                .title(fakeBook.title())
+                .author(fakeBook.author())
+                .publisher(fakeBook.publisher())
+                .genre(fakeBook.genre())
+                .build());
     }
 }
