60fc0da5035c8daa2962b6ebadaaf37c65c2f964
Tiago Angelo <kurtis.angelo@gmail.com>
2022-07-14 19:29:12 -0300

    Add logs
    Signed-off-by: Tiago Angelo <kurtis.angelo@gmail.com>

--- apps/library/src/main/java/com/kurtis/library/LibraryController.java
+++ apps/library/src/main/java/com/kurtis/library/LibraryController.java
@@ -2,12 +2,17 @@ package com.kurtis.library;
 
 import com.github.javafaker.Faker;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+@Log4j2
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api")
@@ -17,12 +22,19 @@ public class LibraryController {
 
     @RequestMapping(value = "/books", method = RequestMethod.GET)
     public List<Book> readBooks() {
-        com.github.javafaker.Book fakeBook = this.faker.book();
-        return List.of(Book.builder()
-                .title(fakeBook.title())
-                .author(fakeBook.author())
-                .publisher(fakeBook.publisher())
-                .genre(fakeBook.genre())
-                .build());
+        Random random = new Random();
+        int max = 5;
+        int min = 2;
+        max = random.nextInt(max - min) + min;
+        log.info("Found {} books", max);
+        return IntStream.range(0, max)
+                .mapToObj(operand -> this.faker.book())
+                .map(fakeBook -> Book.builder()
+                        .title(fakeBook.title())
+                        .author(fakeBook.author())
+                        .publisher(fakeBook.publisher())
+                        .genre(fakeBook.genre())
+                        .build())
+                .collect(Collectors.toList());
     }
 }
