e31ffceee64015ae85539b65752e569838ff4023
Tiago Angelo <kurtis.angelo@gmail.com>
2022-07-17 19:00:00 -0300

    Fix OpenSearch installation

--- env/opensearch.tf
+++ env/opensearch.tf
@@ -17,7 +17,24 @@ spec:
         namespace: ${kubernetes_namespace.fluxcd.metadata[0].name}
   values:
     replicas: 2
-    protocol: http
+    extraInitContainers:
+      - name: init-sysctl
+        image: busybox:latest
+        imagePullPolicy: IfNotPresent
+        command:
+        - sh
+        - -c
+        - |
+          #!/usr/bin/env bash
+          set -euo pipefail
+          CURRENT=`sysctl -n vm.max_map_count`;
+          DESIRED="262144";
+          if [ "$DESIRED" -gt "$CURRENT" ]; then
+              sysctl -w vm.max_map_count=262144;
+          fi;
+        securityContext:
+          runAsUser: 0
+          privileged: true
     config:
       opensearch.yml: |
         cluster.name: opensearch-cluster
@@ -45,13 +62,16 @@ spec:
   chart:
     spec:
       chart: opensearch-dashboards
-      version: 1.2.2
+      version: 1.1.2
       sourceRef:
         kind: HelmRepository
         name: opensearch
         namespace: ${kubernetes_namespace.fluxcd.metadata[0].name}
   values:
-    opensearchHosts: "http://opensearch-cluster-master:9200"
+    imageTag: "1.1.0"
+    opensearchHosts: "http://opensearch-cluster-master.${kubernetes_namespace.opensearch.metadata[0].name}.svc.cluster.local:9200"
+    opensearchAccount:
+      secret: ${kubernetes_secret_v1.opensearch.metadata[0].name}
     ingress:
       enabled: true
       ingressClassName: traefik
@@ -68,6 +88,18 @@ YAML
     kubectl_manifest.fluxcd,
     kubectl_manifest.opensearch_helm_repository
   ]
+}
+
+resource "kubernetes_secret_v1" "opensearch" {
+  metadata {
+    name      = "opensearch"
+    namespace = kubernetes_namespace.opensearch.metadata[0].name
+  }
+  data = {
+    username = "admin"
+    password = "admin"
+    cookie   = "378dc9cf-c1e2-4d62-9f3f-ac52038cbcd9"
+  }
 }
 
 resource "kubernetes_namespace" "opensearch" {
