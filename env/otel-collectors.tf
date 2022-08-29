e9a0aeab35a36334b84fa5a2cb0e89d983e216ff
Tiago Angelo <kurtis.angelo@gmail.com>
2022-08-29 18:50:44 -0300

    Enable spanmetrics processor
    Signed-off-by: Tiago Angelo <kurtis.angelo@gmail.com>

--- env/otel-collectors.tf
+++ env/otel-collectors.tf
@@ -2,8 +2,9 @@ locals {
   opentelemetry_collectors = {
     otlp = {
       config_file = "otel-collectors/otlp.yaml"
+      ports       = [{ name = "prometheus", port = 8889, targetPort = 8889 }]
       env         = {
-        OTLP_ENDPOINT = "jaeger-collector.${kubernetes_namespace_v1.jaeger.metadata[0].name}.svc.cluster.local:4317"
+        OTLP_ENDPOINT = "jaeger-operator-jaeger-collector.${kubernetes_namespace_v1.jaeger.metadata[0].name}.svc.cluster.local:4317"
       }
     }
   }
@@ -18,11 +19,12 @@ resource "kubectl_manifest" "otelcol" {
     kind       = "OpenTelemetryCollector"
     metadata   = { name = each.key, namespace = kubernetes_namespace_v1.opentelemetry.metadata[0].name }
     spec       = {
-      image    = "docker.io/kurtis/otel-collector:v1.0.3"
-      mode     = try(each.value.mode, "deployment")
+      image = "docker.io/kurtis/otel-collector:v1.0.3"
+      mode = try(each.value.mode, "deployment")
       replicas = 1
-      env      = try([for k, v in local.opentelemetry_collectors.otlp.env : { name = k, value = v }], [])
-      config   = file(each.value.config_file)
+      ports = try(each.value.ports, [])
+      env = try([for k, v in local.opentelemetry_collectors.otlp.env : { name = k, value = v }], [])
+      config = file(each.value.config_file)
     }
   })
 
